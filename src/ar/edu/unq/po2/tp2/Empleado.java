a2c67bf5dee1c0e1a4b7a33a755ad7c3721eee8e
walterggomez <walterggomez@gmail.com>
2023-03-28 01:23:08 -0300

    Comienzo a definir las clase Empresa y Empleado

---
+++ src/ar/edu/unq/po2/tp2/Empleado.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tp2;
+
+public class Empleado {
+	private String nombre;
+	private String direccion;
+	
+	
+
+}
---
+++ src/ar/edu/unq/po2/tp2/Empresa.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tp2;
+
+public class Empresa {
+	private String nombre;
+	private String cuit;
+	
+	
+
+}
